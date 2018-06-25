cbf3be69939fe4173bb972598f614f10037ee6e9
Oscar Guerrero <oguerrero@techona.local>
2018-06-25 18:41:56 +0200

    cleaning the code a little

--- modules/blockchain.js
+++ modules/blockchain.js
@@ -2,28 +2,7 @@ var blockMod = require('./block');
 const calHash = require('./calHash');
 var Blocks     = require('../models/block.model');
 
-/*
-var block = (empty, data)=>{
-  if(empty){
-    return new blockMod.Block(0, data.timestamp, 'Genesis Block', '0');
-  }else{
-    Blocks.find({}, null, {sort: '-date'}, function(err, docs){
-      if(err){
-        console.log(err);
-        return;
-      }else{
-        lastIndex = docs[docs.length - 1].index;
-        previousHash = docs[docs.length - 1].hash;
-        console.log(lastIndex);
-        console.log(previousHash);
-        return new blockMod.Block(lastIndex + 1, data.timestamp, {amount: data.amount, login: data.login}, previousHash);
-      }
-    });
-  }
-};
 
-*/
-// data = { amount: 4, login: 'pepe', timestamp: 1529938235811 }
 var block = function (empty, data){
   return new Promise(function(resolve, reject){
     if(empty){
@@ -36,8 +15,6 @@ var block = function (empty, data){
         }else{
           lastIndex = docs[docs.length - 1].index;
           previousHash = docs[docs.length - 1].hash;
-          console.log(lastIndex);
-          console.log(previousHash);
           resolve(new blockMod.Block(lastIndex + 1, data.timestamp, {amount: data.amount, login: data.login}, previousHash));
         }
       });
